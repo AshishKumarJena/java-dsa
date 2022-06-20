77541dca87ca160a7e6a9e90ffaae8100d6f1caf
AshishKumarJena <ashishkujena2000@gmail.com>
2022-06-21 02:39:24 +0530

    New Commit

---
+++ DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+
+    public static void fun() {
+        int temp = a;
+        a = b;
+        b = temp;
+
+    }
+
+    static int a = 10;
+    static int b = 20;
+
+    public static void main(String[] args) {
+        fun();
+        System.out.println(a + " " + b);
+
+    }
+}
---
+++ InputOutput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        // int n = scn.nextInt();
+        // System.out.println("Your input was " + n);
+        // System.out.print("Hello World");
+        // System.out.print("Hello World");
+
+        // String s = scn.nextLine();
+        String s = scn.next();
+
+        System.out.println(s);
+
+        // for, while, do-while, break, continue
+        // if, else, if-else, switch
+
+    }
+}
