c83e42981bf5bf390a243f1313bdfd42dc6249f6
AshishKumarJena <ashishkujena2000@gmail.com>
2022-06-22 10:47:21 +0530

    New Commit

---
+++ Print1To10.java
@@ -0,0 +1,11 @@
+public class Print1To10 {
+    
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=9){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Done");
+    }
+}
