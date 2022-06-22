a4a08077b195b5dd7718436037218d2e11865469
AshishKumarJena <ashishkujena2000@gmail.com>
2022-06-22 11:50:19 +0530

    New Commit

---
+++ Input0To5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Input0To5 {
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+        for (int i = 0; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ Print1To10ForLoop.java
@@ -0,0 +1,10 @@
+public class Print1To10ForLoop {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Done");
+    }
+}
