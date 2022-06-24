eb953aa1960ad4a642fb621c0be5ad1c9c07f5ee
AshishKumarJena <ashishkujena2000@gmail.com>
2022-06-24 21:44:39 +0530

    New Commit

---
+++ StackProg.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class StackProg {
+    
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        System.out.println(st + "->" + st.peek() + " " + st.size());
+        st.push(20);
+        System.out.println(st + "->" + st.peek() + " " + st.size());
+        st.push(30);
+        System.out.println(st + "->" + st.peek() + " " + st.size());
+        st.push(40);
+        System.out.println(st + "->" + st.peek() + " " + st.size());
+        st.pop();
+        System.out.println(st + "->" + st.peek() + " " + st.size());
+        st.pop();
+        System.out.println(st + "->" + st.peek() + " " + st.size());
+        st.pop();
+        System.out.println(st + "->" + st.peek() + " " + st.size());
+        st.pop();
+        System.out.println(st + "->" + " " + st.size());
+    }
+}
